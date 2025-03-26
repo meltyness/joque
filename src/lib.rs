24a37ac7f0a3695a0a29ddff6c7b46fd63e7569d
meltyness <86126050+meltyness@users.noreply.github.com>
2025-03-26 17:38:49 -0500

    before having written a gc

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,112 @@
-use std::sync::atomic::{AtomicPtr, AtomicU32, AtomicUsize, Ordering};
+use std::{sync::atomic::{AtomicPtr, AtomicU32, AtomicUsize, Ordering}, thread};
 
 /// Joque implements a lock-free double-ended queue.
 #[allow(dead_code)]
 struct Joque<T> {
     // Contains an op_id muxed with a "pointer" into backing
-    deque: [AtomicUsize; 50],
-    pub left: AtomicUsize,  // left side of deque
-    right: AtomicUsize, // right side of deque
+    deque: Vec<AtomicUsize>,
+    leftright: AtomicUsize,
     capacity: usize, // size of heap
     
     // 💡✨: 128 entry chunks, with 128/bitwise find empty, NO GC. 
-    backing: [AtomicPtr<T>; 100], // zero is the null ptr in this reference frame
+    backing: Vec<AtomicPtr<T>>, // zero is the null ptr in this reference frame
     op_id: AtomicU32,               
     idx: AtomicU32,
 }
+
+const LEFTMASK: usize = 0x00000000_FFFFFFFF;
+const RIGHTMASK: usize = 0xFFFFFFFF_00000000;
+const ONE: usize = 0x00000001_00000000;
+
 #[allow(dead_code)]
 impl<T> Joque<T> {
-    pub fn new() -> Self {
+    pub fn new(width: usize) -> Self {
+        if width < 10 { panic! ("let's not"); }
+        let left = width / 2;
+        let right = (left + 1) << 32;
         Joque {
-            deque: [const { AtomicUsize::new(0) }; 50], // TODO: 💀 dynamically resizable
-            left: AtomicUsize::new(25),
-            right: AtomicUsize::new(26),
-            capacity: 50,
-            backing: [const { AtomicPtr::new(std::ptr::null_mut()) }; 100], // TODO: 💀 dynamically resizable
+            deque: std::iter::from_fn(|| Some(AtomicUsize::new(0))).take(width).collect(), // TODO: 💀 dynamically resizable
+            leftright: AtomicUsize::new(left | right),
+            capacity: width,
+            backing: std::iter::from_fn(|| Some(AtomicPtr::new(std::ptr::null_mut()))).take(width*4).collect(), // TODO: 💀 dynamically resizable
             op_id: AtomicU32::new(0),
             idx: AtomicU32::new(1),
         }
     }
+
     pub fn push_front(&self, item: Box<T>) {
-        let back_idx = self.idx.fetch_add(1, Ordering::SeqCst); // TODO: 💀 after 400 write/read cycles
-        self.backing[back_idx as usize].store(Box::into_raw(item), Ordering::SeqCst);
+        // reserve backing storage
+        //  - unique until wrapped
+        let backing_idx = self.idx.fetch_add(1, Ordering::Acquire); // TODO: 💀 after 400 write/read cycles
+
+        // immediately write deposited value into backing storage
+        //  - no fallible push
+        self.backing[backing_idx as usize].store(Box::into_raw(item), Ordering::SeqCst);
+
+        // for each CAS retry
         loop {
-            let this_left = self.left.load(Ordering::SeqCst);
-            let lval = self.deque[this_left].load(Ordering::SeqCst);
-            let entry = ((self.op_id.fetch_add(1, Ordering::SeqCst) as usize) << 32) | back_idx as usize;
+            // fetch the current target index
+            let this_left = self.fetch_extent_acq().0;
+
+            // fetch the associated backing location
+            let lval = self.deque[this_left].load(Ordering::Acquire);
+
+            // increment and fetch op id, mux with backing_idx
+            let entry = ((self.op_id.fetch_add(1, Ordering::Acquire) as usize) << 32) | backing_idx as usize;
+
             // break deque's neck to point at our crap
-            if self.deque[this_left].compare_exchange(lval, entry, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
-                // println!("Dropped into {back_idx}, ok");
-                self.left.fetch_sub(1, Ordering::SeqCst);
+            if self.deque[this_left].compare_exchange(lval, entry, Ordering::Acquire, Ordering::Acquire).is_ok() {
+                
+                // println!("Pushed onto {}, ok", entry & LEFTMASK);
+
+                // we know that we succeeded, but this isn't necessarily synchronized
+                self.leftright.fetch_sub(1, Ordering::Acquire);
                 break;
             }
         }
     }
 
-    pub fn pop_front(&self) -> Box<T> {
+    pub fn pop_front(&self) -> Option<Box<T>> {
         loop {
-            let this_left = self.left.load(Ordering::SeqCst) + 1;
-            let lval = self.deque[this_left].load(Ordering::SeqCst);
+            let (sens_left, sens_right) = self.fetch_extent_rel();
+            if sens_left + 1 >= sens_right { return None; }
+            let this_left = sens_left + 1;
+            let lval = self.deque[this_left].load(Ordering::Acquire);
+            if lval & LEFTMASK == 0 { thread::yield_now(); return None; }
             let idx = 0;
-            let entry = ((self.op_id.fetch_add(1, Ordering::SeqCst) as usize) << 32) | idx;
-            if let Ok(_) = self.deque[this_left].compare_exchange(lval, entry, Ordering::SeqCst, Ordering::SeqCst) {
-                // println!("Seeking from {}, ok", lval & 0xFFFFFFFF);
-                let out = self.backing[(lval & 0xFFFFFFFF) as usize].swap(std::ptr::null_mut(), Ordering::SeqCst );
-                self.left.fetch_add(1, Ordering::SeqCst);
+            let entry = ((self.op_id.fetch_add(1, Ordering::Release) as usize) << 32) | idx;
+            if let Ok(old_one) = self.deque[this_left].compare_exchange(lval, entry, Ordering::Release, Ordering::Relaxed) {
+                // println!("Seeking from {}, ok", lval & LEFTMASK);
+                let out = self.backing[(lval & LEFTMASK) as usize].swap(std::ptr::null_mut(), Ordering::Release );
+                self.leftright.fetch_add(1, Ordering::Release);
                 unsafe { 
-                    return Box::from_raw(out);
+                    if out.is_null() {
+                        println!("Well that was weird I couldn't pull {}", lval & LEFTMASK);
+                        println!("Here's some other stuff: ");
+                        println!("Directed pop state: {this_left}");
+                        println!("Claimed Op ID: {}", entry >> 32);
+                        println!("Old Cmp Data: {} {}", old_one & RIGHTMASK >> 32, old_one & LEFTMASK);
+                        return None;
+                    } else {
+                        return Some(Box::from_raw(out));
+                    }
                 }
             }
         }
+    }
+
+    fn fetch_extent_acq(&self) -> (usize, usize) {
+        let muxed = self.leftright.load(Ordering::Acquire);
+        let left_demuxed = muxed & LEFTMASK;
+        let right_demuxed = (muxed & RIGHTMASK) >> 32;
+        (left_demuxed, right_demuxed)
+    }
+
+    fn fetch_extent_rel(&self) -> (usize, usize) {
+        let muxed = self.leftright.load(Ordering::Relaxed);
+        let left_demuxed = muxed & LEFTMASK;
+        let right_demuxed = (muxed & RIGHTMASK) >> 32;
+        (left_demuxed, right_demuxed)
     }
 
     pub fn get(&self, _index: usize) -> Option<usize> {
@@ -85,19 +134,19 @@ impl<T> Joque<T> {
 
 mod tests {
     use std::sync::atomic::Ordering;
-    use crate::Joque;
+    use crate::{Joque, LEFTMASK};
 
     #[test]
     pub fn basic_test() {
-        let deque = Joque::new();
+        let deque = Joque::new(25);
 
         deque.push_front(Box::new("squirpy"));
         deque.push_front(Box::new("squirp"));
         deque.push_front(Box::new("squirp"));
 
-        assert_eq!("squirp", *deque.pop_front());
-        assert_eq!("squirp", *deque.pop_front());
-        assert_eq!("squirpy", *deque.pop_front());
+        assert_eq!("squirp", *deque.pop_front().unwrap());
+        assert_eq!("squirp", *deque.pop_front().unwrap());
+        assert_eq!("squirpy", *deque.pop_front().unwrap());
     }
 
     use loom::sync::Arc;
@@ -108,8 +157,10 @@ mod tests {
     #[test]
     fn permute_interleaved_modification() {
         let THREAD_COUNT = 4;
+        let WIDTH = 25;
+        let LEFT_START = WIDTH / 2;
         loom::model(move || {
-            let deque = Arc::new(Joque::new());
+            let deque = Arc::new(Joque::new(WIDTH));
             
             let ths: Vec<_> = (0..THREAD_COUNT).map(|idx| {
                 let big_deque = deque.clone();
@@ -126,8 +177,54 @@ mod tests {
                 th.join().unwrap();
             }
 
-            assert_eq!(25 - THREAD_COUNT*4, deque.clone().left.load(Ordering::Relaxed));
+            assert_eq!(LEFT_START - THREAD_COUNT*2, deque.clone().leftright.load(Ordering::Relaxed) & LEFTMASK);
         });
+    }
+
+    #[test]
+    fn interleaved_modification() {
+        let THREAD_COUNT = 64;
+        let PAD_WIDTH = 16;
+        let WIDTH = 512;
+        let LEFT_START = WIDTH / 2;
+        let RERUNS = 10000;
+        
+        for rerun in 0..RERUNS {
+            println!("~~~~~ {rerun} ~~~~~");
+            let deque = std::sync::Arc::new(Joque::new(WIDTH));
+
+            for _ in 0..PAD_WIDTH {
+                deque.push_front(Box::new(usize::MAX));
+            }
+
+            let mut ths: Vec<_> = (0..THREAD_COUNT/2).map(|idx| {
+                let big_deque = deque.clone();
+
+                std::thread::spawn( move || {
+                    big_deque.push_front(Box::new(idx));
+                    while big_deque.pop_front().is_none() { }
+                    big_deque.push_front(Box::new(idx+1));
+                    big_deque.push_front(Box::new(idx+2));
+                })})
+                .collect();
+
+            ths.append(&mut (0..THREAD_COUNT/2).map(|idx| {
+                let big_deque = deque.clone();
+
+                std::thread::spawn( move || {
+                    big_deque.push_front(Box::new(idx));
+                    big_deque.push_front(Box::new(idx+1));
+                    while big_deque.pop_front().is_none() { }
+                    big_deque.push_front(Box::new(idx+2));
+                })})
+                .collect());
+
+            for th in ths {
+                th.join().unwrap();
+            }
+
+            assert_eq!(LEFT_START - THREAD_COUNT*2 - PAD_WIDTH, deque.clone().leftright.load(Ordering::Relaxed) & LEFTMASK);
+        }
     }
 
     #[test]
